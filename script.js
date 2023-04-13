36e9b76fc682790098a4960b40e2a5606c8d8f23
Kabannikov Konstantin <kabannikov-k@mai.ru>
2023-04-13 18:40:50 +0600

    added check input data

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ let appData = {
   expenses: {},
   addExpenses: [],
   deposit: false,
+  percentDeposit: 0,
+  moneyDeposit: 0,
   mission: 70000,
   period: 3,
   budgetDay: 0,
@@ -29,18 +31,64 @@ let appData = {
   month: 30,
 
   asking: function () {
-    let addExpenses = prompt(
+    if (confirm("Есть ли у вас дополнительный заработок?")) {
+      let itemIncome = prompt(
+        "Какой у вас есть дополнительный заработок?",
+        "Таксую"
+      );
+
+      while (
+        isNumber(itemIncome) ||
+        itemIncome.length < 1 ||
+        itemIncome.trim() === ""
+      ) {
+        itemIncome = prompt(
+          "Какой у вас есть дополнительный заработок?",
+          "Таксую"
+        );
+      }
+
+      let cashIncome = prompt("Сколько в месяца зарабатываете на этом?", 10000);
+
+      while (!isNumber(cashIncome)) {
+        cashIncome = prompt("Сколько в месяца зарабатываете на этом?", 10000);
+      }
+
+      appData.income[itemIncome] = cashIncome;
+    }
+
+    let data = prompt(
       "Перечислите возможные расходы за рассчитываемый период через запятую"
     );
 
-    appData.addExpenses = addExpenses.toLowerCase().split(",");
+    let dataArr = data.split(",");
+
+    for (let i = 0; i < dataArr.length; i++) {
+      appData.addExpenses.push(dataArr[i]);
+    }
+
+    for (let i = 0; i < dataArr.length; i++) {
+      appData.addExpenses[i] = appData.addExpenses[i].trim();
+      appData.addExpenses[i] =
+        appData.addExpenses[i][0].toUpperCase() +
+        appData.addExpenses[i].slice(1);
+    }
+
+    appData.addExpenses = appData.addExpenses.join(", ");
+
     appData.deposit = confirm("Есть ли у вас депозит в банке?");
 
     for (let i = 0; i < 2; i++) {
       let number;
-      let question;
+      let question = prompt("Введите обязательную статью расходов:");
 
-      question = prompt("Введите обязательную статью расходов:");
+      while (
+        isNumber(question) ||
+        question.length < 1 ||
+        question.trim() === ""
+      ) {
+        question = prompt("Введите обязательную статью расходов:");
+      }
 
       while (!isNumber(number)) {
         number = prompt("Сколько это будет стоить:");
@@ -71,9 +119,7 @@ let appData = {
     if (Math.ceil(appData.mission / appData.budgetMonth) < 0) {
       appData.period = "Цель не будет достигнута";
     } else {
-      appData.period =
-        "В течение этого времени сумма будет накоплена(месяцы) " +
-        Math.ceil(appData.mission / appData.budgetMonth);
+      appData.period = Math.ceil(appData.mission / appData.budgetMonth);
     }
   },
 
@@ -88,6 +134,26 @@ let appData = {
       appData.income = "Что-то пошло не так";
     }
   },
+
+  getInfoDeposit: function () {
+    if (appData.deposit) {
+      appData.percentDeposit = prompt("Какой годовой процент?", "10");
+
+      while (!isNumber(appData.percentDeposit)) {
+        appData.percentDeposit = prompt("Какой годовой процент?", "10");
+      }
+
+      appData.moneyDeposit = prompt("Какая сумма заложена?", 10000);
+
+      while (!isNumber(appData.moneyDeposit)) {
+        appData.moneyDeposit = prompt("Какая сумма заложена?", 10000);
+      }
+    }
+  },
+
+  calcSavedMoney: function () {
+    return appData.budgetMonth * appData.period;
+  },
 };
 
 console.log(appData);
@@ -99,13 +165,15 @@ appData.getTargetMonth();
 appData.getStatusIncome();
 
 console.log("Расходы за месяц равны: " + appData.expensesMonth);
-console.log(appData.period);
+console.log(
+  "В течении этого времени будет накоплена сумма (месяцы) " + appData.period
+);
 console.log(
   "Бюджет на день исходя из месячного накопления равен , " + appData.budgetDay
 );
 
 for (let key in appData) {
-  if (typeof appData[key] != "function") {
+  if (typeof appData[key] !== "function") {
     console.log(
       "Наша прогамма включает в себя данные: " + key + " : " + appData[key]
     );
@@ -113,3 +181,12 @@ for (let key in appData) {
     console.log("Данное свойство содержит в себе функцию");
   }
 }
+
+appData.getInfoDeposit();
+console.log(
+  appData.percentDeposit,
+  appData.moneyDeposit,
+  appData.period,
+  appData.calcSavedMoney()
+);
+console.log(appData.addExpenses);
